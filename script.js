d2a95a0fe793ea16fed2c832b9808997e726f6ca
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2023-05-30 15:54:25 +0200

    fetch

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ L.tileLayer('https://hisgis.nl/wmts/minuutplans/cut/{z}/{x}/{y}', {
     maxZoom: 19,
     attribution: 'HisGIS'
 }).addTo(map);
+
+document.getElementById("checkvenster").onclick = function(){
+    let url = "https://osm.hisgis.nl/api/0.6/map.json?bbox=";
+    url += map.getBounds().toBBoxString();
+    document.getElementById("bericht").innerHTML += map.getBounds().toBBoxString();
+    const response = await fetch(url);
+    const jsonData = await response.json();
+    console.log(jsonData);
+}
