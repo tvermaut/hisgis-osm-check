aa1c9abaaa8d860b7849758d4e8a037fe0a4bdaa
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2023-05-30 19:51:32 +0200

    i

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ function verwerk(j){
                             var ps = [];
                             for (const i of (osm.ways[m.ref]).getLatLngs()){
                                 let p = osm.nodes[i];
-                                console.log(p);
+                                console.log(i);
                                 ps.push([p.getLatLng().lat, p.getLatLng().lng]);
                             }
                         rs[m.role].push(ps);
